99858378a62303818c8829e8a2e7dbe4ac37969d
Jhilon Valentin <jneciosupj@autonoma.edu.pe>
2025-07-15 16:18:13 -0500

    feat: seguridad mejorada, MONGO_URI por variable de entorno, formulario BI completo

---
+++ run.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+
+
+def run_flask():
+    os.chdir('app')
+    subprocess.run(['python', 'app.py'])
+
+def run_etl():
+    os.chdir('etl')
+    subprocess.run(['python', 'etl.py'])
+
+
+def main():
+    print("¿Qué deseas ejecutar?")
+    print("1. Flask (app web)")
+    print("2. ETL (proceso de datos)")
+    op = input("Elige (1/2): ")
+    if op == "1":
+        run_flask()
+    elif op == "2":
+        run_etl()
+    else:
+        print("Opción no válida.")
+
+if __name__ == "__main__":
+    main() 
